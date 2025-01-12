faa5d0a1dd92e87359f3cbf8ca1c12d4efbcabf1
Anim Michael Asante <160376570+Obofour@users.noreply.github.com>
2025-01-12 00:38:40 +0000

    first commit

---
+++ public/main.js
@@ -0,0 +1,21 @@
+const initApp=()=>{
+    const hamburgerBtn=document.getElementById('hamburger-button')
+    const mobileMenu=document.getElementById('mobile-menu')
+
+    const toggleMenu=()=>{
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+        hamburgerBtn.classList.toggle('toggle-btn')
+    }
+
+    hamburgerBtn.addEventListener('click',toggleMenu)
+    mobileMenu.addEventListener('click',toggleMenu)
+}
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+    dropdown.addEventListener('click', function() {
+        this.querySelector('.dropdown-menu').classList.toggle('hidden');
+    });
+});
+
+document.addEventListener('DOMContentLoaded',initApp)
+document.getElementById('date').innerHTML=new Date().getFullYear()
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./public/**/*.{html,js,ts,jsx,tsx,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
