55ed0b81cd8205ac1cdd3f2998458452e89d52af
Anim Michael Asante <160376570+Obofour@users.noreply.github.com>
2025-01-12 02:30:16 +0000

    correction of hambuger
    i add a js code to make the hambuger function

--- public/main.js
+++ public/main.js
@@ -1,16 +1,18 @@
-const initApp=()=>{
-    const hamburgerBtn=document.getElementById('hamburger-button')
-    const mobileMenu=document.getElementById('mobile-menu')
+const initApp = () => {
+    const hamburgerBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
 
-    const toggleMenu=()=>{
+    const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden')
         mobileMenu.classList.toggle('flex')
         hamburgerBtn.classList.toggle('toggle-btn')
     }
 
-    hamburgerBtn.addEventListener('click',toggleMenu)
-    mobileMenu.addEventListener('click',toggleMenu)
+    hamburgerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener('click', toggleMenu)
 }
+
+document.addEventListener('DOMContentLoaded', initApp)
 document.querySelectorAll('.dropdown').forEach(dropdown => {
     dropdown.addEventListener('click', function() {
         this.querySelector('.dropdown-menu').classList.toggle('hidden');
